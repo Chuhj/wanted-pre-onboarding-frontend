f64f84762680cecc1a2f488b131e790ce6f8dc59
Chuhj <chj9574@naver.com>
2023-04-14 23:01:59 +0900

    배포를 위해 BrowserRouter에 basename 추가

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Todo from './pages/Todo';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Routes>
           <Route path="/" element={<Navigate to={'/signin'} replace />} />
